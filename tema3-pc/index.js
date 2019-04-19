79e19317476227fd62fdb9b66fd8cd328e2f4304
vanhaaggen <vanhaaggen@gmail.com>
2019-04-19 20:16:41 +0200

    Add random unic number generator

--- tema3-pc/index.js
+++ tema3-pc/index.js
@@ -6,17 +6,20 @@ let s4 = ['font-size: 18px', 'background-color: #3b3a3a ', 'color: #00ff77'].joi
 let s5 = ['font-size: 18px', 'text-decoration: underline', 'color: #3b3a3a'].join(';')//info
 
 
-
 const bingo = {
-    _data:{
+    _data: {
         _user: {name:[], balls:[], points: 0},
-        _score:[],
-        _carton: {original: [], copy:[]},
+        _carton: {original: [], copy: []},
+        _score: [],
         _randomNum: [],
+        _cache: [],
+        _randomBall: [],
         _trigger :{ oneLine: true, 
-            twoLines: true,
-            threeLines : true},
-     
+                    twoLines: true,
+                    threeLines : true,
+                    gameStatus: true},
+        
+
         get user(){
             return this._user;
         },
@@ -26,16 +29,15 @@ const bingo = {
             this._user.points = userData;
         },
 
-        get score(){
-            return this._score;
-        },
-    
         get carton(){
             return this._carton;
         },
-
         set carton(newCarton){
            this._carton.copy = newCarton;
+        },
+
+        get score(){
+            return this._score;
         },
 
         get randomNum(){
@@ -44,6 +46,20 @@ const bingo = {
         set randomNum(randomNumIn){
             this._randomNum = randomNumIn;
         },
+        
+        get cache(){
+            return this._cache;
+        },
+        set cache(cacheIn){
+            this._cache = cacheIn;
+        },
+
+        get randomBall(){
+            return this._randomBall;
+        },
+        set randomBall(randomBallIn){
+            this._randomBall = randomBallIn;
+        },
 
         get trigger(){
             return this._trigger;
@@ -51,14 +67,14 @@ const bingo = {
         set trigger(triggerIn){
             this._trigger = triggerIn
         }
+
     },
-    
+
     triggerStat(boolean){
         this._data.trigger.oneLine = boolean
         this._data.trigger.twoLines = boolean
         this._data.trigger.threeLines = boolean
       },
-
       
     addGamertoRanking(name, balls, points){
         let users = {
@@ -75,7 +91,6 @@ const bingo = {
         for(let value in originalObj) objCopy[value] = originalObj[value]
     },
 
-    
     getUserName(){
         let n = prompt('Whats your Name?');
         if(!n){1
@@ -85,45 +100,57 @@ const bingo = {
             console.log(`%cHi ${n === '' ? 'Stranger' : n}!\nWellcome to %c Balyks BINGO 🍀🍀🍀`,s1,s2)   
         };
     },
-
-    getRandomNumber(){
-        let randomize = Math.floor(Math.random()* 15 + 1);
-        this._data.randomNum = randomize;
-        this._data.user.balls++
-       
-    },
-
-    getNewCarton(){
-        let numArr = []; 
+    //--------Fisher & Yates shuffle algorithm-------------------------
+    shuffle(){
+        let numArr = this._data.randomNum;
         let count = 1;
         for(let i = 0 ; i < 50; i++){
-                numArr.push(count++)
+            this._data.randomNum.push(count++)
         };
-        //Fisher and Yates shuffle algorithm-----------------------
         let currentIndex = numArr.length, temporaryValue, randomIndex;
         while(0 !== currentIndex){
-         
-          randomIndex = Math.floor(Math.random()* currentIndex)
-          currentIndex -= 1;
-          
-          temporaryValue = numArr[currentIndex];
-          numArr[currentIndex] = numArr[randomIndex];
-          numArr[randomIndex] = temporaryValue; 
-        }
-        //----------------------------------------------------------
-        let newCarton = numArr.slice(0, 15)
+        
+        randomIndex = Math.floor(Math.random()* currentIndex)
+        currentIndex -= 1;
+        
+        temporaryValue = numArr[currentIndex];
+        numArr[currentIndex] = numArr[randomIndex];
+        numArr[randomIndex] = temporaryValue; 
+        };
+    },
+    //-------------------------------------------------------------
+    getNewCarton(){
+        let arrCopy = [...this._data.randomNum]
+        let newCarton = arrCopy.slice(0, 15)
         newCarton = newCarton.flat();
         while (newCarton.length) {
             this._data.carton.original.push(newCarton.splice(0,5));
         }; 
         let w = this._data.carton.original
         console.log(`%cYour Ticket\n%c${w.map(x => `|${x.join('|')}|`).join('\n')}`,s5,s3)
+
     },
-    //Array.from({length: 15}, (v, i) => Math.floor(Math.random() * 15 + 1));
+
+    copyToCache(){
+        this._data.cache = [...this._data.randomNum]
+    },
+
+    getRandomBalls(){
+        let cacheArr = this._data.cache
+        if(cacheArr.length > 0){
+            let index = Math.floor(Math.random() * cacheArr.length)
+            let ball = cacheArr.splice(index, 1);
+            this._data.randomBall = ball;
+            this._data.user.balls++
+        }else{
+            console.log('Empty cage!')
+            
+        }
+    },
 
     points(){
         let c = this._data.carton.copy;
-        let r = this._data.randomNum;
+        let r = this._data.randomBall[0];
         let flat = c.flat();
         
         c.map(arr => arr.map(num => num === r ? this._data.user.points += 2 : ''))
@@ -141,21 +168,20 @@ const bingo = {
         };
 
         if(this._data.trigger.threeLines && flat.length === 3 ){
-            this._data.trigger.twoLines = false;
+            this._data.trigger.threeLines = false;
             this._data.user.points += 15;
-        } 
+        };
       
-
     },
 
     checkIfMatch(){  
         let c = this._data.carton.copy;
-        let r = this._data.randomNum;
+        let r = this._data.randomBall[0];
         let p = this._data.user.points;
         let flat = c.flat();
         this.points();
 
-        console.log(`%cBall number: ${this._data.randomNum}\n\n%c${c.map((arr, i) => `|${arr.map((num, j) => num === r ? c[i][j] = 'X' : num).join('|')}|`).join('\n')}`,s5,s3);
+        console.log(`%cBall number: ${this._data.randomBall}\n\n%c${c.map((arr, i) => `|${arr.map((num, j) => num === r ? c[i][j] = 'X' : num).join('|')}|`).join('\n')}`,s5,s3);
         if(this._data.user.points > p) console.log(`Points: +${this._data.user.points - p}  Total: ${this._data.user.points}`);
         if(this._data.user.points === p){
             this._data.user.points -= 1;
@@ -168,37 +194,28 @@ const bingo = {
             print = console.log(`%c ---BINGO---\n%c🍀👏👏🍀\non round ${this._data.user.balls - 1}`,s4,s1)
             this._data.carton.original.length = 0;
             this._data.carton.copy.length = 0;
+            this._data.trigger.gameStatus = false;
             this.addGamertoRanking(this._data.user.name, this._data.user.balls - 1, this._data.user.points);
             this.newGame();
-           };
-
-         
+           
+           };    
     },
 
+    scoreTable(){
+        this._data.score.forEach(prop => console.log(`%cName: ${prop.name}, Balls: ${prop.balls}, Points: ${prop.points}`,s1 ));
+        let filter =  Object.values(this._data.score).filter(val=> val.points ).sort((a, b) => b.points - a.points).slice(0,1);
+        filter.forEach(item => console.log(`%cThe Winner is: ${item.name}, with ${item.balls} balls and a total of ${item.points} points`,s3));
+      
+    },
 
-   newBall(){
-       let con = confirm(`New Ball?`) 
-            if(con){
-              this.playGame();
-            }else{
-                return false;
-            }  
-   },
-
-   scoreTable(){
-       this._data.score.forEach(prop => console.log(`%cName: ${prop.name}, Balls: ${prop.balls}, Points: ${prop.points}`,s5 ))
-       let filter =  Object.values(this._data.score).filter(val=> val.points ).sort((a, b) => b.points - a.points).slice(0,1)
-       filter.forEach(item => console.log(`%cThe Winner is: ${item.name}, with ${item.balls} balls and a total of ${item.points} points`,s3))
-     
-   },
-
-
-   newGame(){
+    
+    newGame(){
         let nw = prompt(`1----New Game?\n2----New Player?\n3----Score Table\n4----Exit Game`)
         if(nw){
             this.triggerStat(true)
             this._data.user.balls = 0
             this._data.user.points = 0
+            this._data.randomNum.length = 0
            if(nw === '1'){
                 this.chooseTicket();
            }else if(nw === '2'){
@@ -207,11 +224,11 @@ const bingo = {
                 this.scoreTable();
                 this.newGame(); 
            }else if(nw === '4') 
-           return false;
+                return ;
        }else if(!nw){
            this.newGame();
        }else{
-           return false
+           return;
        };
    },
 
@@ -225,14 +242,17 @@ const bingo = {
    },
 
    chooseTicket(){
+        this.shuffle();
         this.getNewCarton()
         this.copyObj()
         let c = prompt(`Are you happy with this Ticket?\n1--YES\n2--NO`)
         if(c){
             if(c === '1'){
+                this.copyToCache()
                 this._data.user.points === 0 ? this._data.user.points = 100 : this._data.user.points = 100
                 this.playGame()
             }else if(c === '2'){
+                this._data.randomNum.length = 0
                 this._data.carton.original.length = 0
                 this.chooseTicket()
             };
@@ -241,7 +261,19 @@ const bingo = {
         };
    },
 
+    newBall(){
+        if(this._data.trigger.gameStatus == true){
+            let con = confirm(`New Ball?`) 
+                if(con == true){
+                this.playGame();
+                }else{
+                return false;
+                }
+            }   
+    },
+
    gameFlow(){
+        this._data.trigger.gameStatus = true;
         this._data.carton.original.length = 0;
         this._data.carton.copy.length = 0;
         this.getUserName();
@@ -249,11 +281,11 @@ const bingo = {
     },
 
     playGame(){
-        this.getRandomNumber();
+        this.getRandomBalls();
         this.checkIfMatch();
         this.newBall();   
     },
-    
-};
 
+
+}
 bingo.gameFlow()
